f43c6fc8a937c8db9c61607e91d1b6258feb2b92
zsh <zhangshenhao@outlook.com>
2022-05-06 23:42:25 +0800

    update

--- apps/df2rdd.py
+++ apps/df2rdd.py
@@ -1,6 +1,9 @@
 import pandas as pd
+from pyspark.sql import DataFrame
+from pyspark.rdd import RDD
+from pyspark import Row
 
-def df_to_rdd(df, groupby_key,numPartitions=50):
+def table_to_PandasDF_rdd(table:DataFrame, groupby_key:list):
     """
     将SparkDataFrame转换为分组的RDD
 
@@ -19,19 +22,34 @@ def df_to_rdd(df, groupby_key,numPartitions=50):
         Key-Value RDD，Key为分组的字段，Value为Pandas DataFrame
     """
     # 防止groupby_key为单个字符串
-    if not isinstance(groupby_key,list):
-        groupby_key = [groupby_key]
+    groupby_key = groupby_key if isinstance(groupby_key,list) else [groupby_key]
         
-    rdd = df.rdd
+    rdd = table.rdd
     out_rdd = (
         rdd
-        .repartition(numPartitions)
         .keyBy(lambda row: tuple([row[key] for key in groupby_key]))
-        .mapValues(lambda row: [row.asDict()])
-        .reduceByKey(lambda r1, r2: r1+r2)
-        .mapValues(lambda r: pd.DataFrame(r))
+        .mapValues(lambda row: row.asDict())
+        .groupByKey()
+        .mapValues(pd.DataFrame)
     )
     return out_rdd
 
-def rdd_to_df(rdd):
-    pass
+def PandasDF_rdd_to_table(rdd:RDD):
+    """
+    将Pandas DataFrame的RDD转换为pyspark的DataFrame
+
+    Parameters
+    ----------
+    parid_rdd : RDD
+        value是pandas DataFrame的RDD
+
+    Returns
+    -------
+    pyspark DataFrame
+        数据
+    """
+    # TODO 输入可以不是pair rdd
+    # TODO 输入是pari rdd时，保留key
+    rdd = rdd.flatMapValues(lambda df:map(lambda row:Row(**row[1].to_dict()),df.iterrows()))
+    rdd = rdd.map(lambda rdd:rdd[1])
+    return rdd.toDF()
